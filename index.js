64bc282fa946dd6245ab74b6f6f7704e4c46f6cb
Nicole Serras <n.serras123@gmail.com>
2020-09-11 17:28:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
 Function sayHiToGrandma(string) {
 if (string.toLowerCase() === string) {
 return ("I can't hear you!");
